50e76c37d178ac6846202441e2d900f3924f45f5
Ivana Stojadinović <ivanastojadinovic001@gmail.com>
2024-03-08 18:43:37 +0100

    spelling

--- src/pages/Chatroom.jsx
+++ src/pages/Chatroom.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Logout from '../components/Logout'
 import ChatBox from '../components/ChatBox'
-import '../styles/Chat.css'
+import '../styles/Chatroom.css'
 
 const Chat = () => {
     const [visibility, setVisibility] = useState(false)
