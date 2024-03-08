3c1520f8125eb80476964f0a7df0d0f04f7cf085
Ivana Stojadinović <ivanastojadinovic001@gmail.com>
2024-03-08 18:25:17 +0100

    name change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Chat from './pages/Chat';
+import Chat from './pages/Chatroom';
 
 function App() {
   return (
